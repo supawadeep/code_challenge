128b60d3ae8606ec20339e46eeab37dec5aea41c
squire <sc.supawadeep@gmail.com>
2021-02-24 20:50:15 +0700

    init

---
+++ Code_Challenge_supawadee.py
@@ -0,0 +1,24 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+way = []
+start = input("What is start node?: ")
+print(start)
+way.append(start)
+print(way)
+goal = input("What is goal node?: ")
+way.append(goal)
+cost = 0
+resWay = '->'.join(way)
+
+# TODO :: กำหนดจุดต่างๆ ของ set
+set v = {'A', 'B'}
+
+# TODO :: Search เข้าไปใน node ว่า node ไหน สั้นที่สุด
+tempCost 
+
+f = open("graph.csv", "r")
+
+print(f.read())
+
+print("Path from", start, 'to', goal, 'is', resWay)
