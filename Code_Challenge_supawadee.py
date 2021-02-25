b237ce40dbbaa8a4cb0c5b8f71eaec82cc7fa40a
supawadeep <67038527+supawadeep@users.noreply.github.com>
2021-02-25 21:31:36 +0700

    update code > set data

--- Code_Challenge_supawadee.py
+++ Code_Challenge_supawadee.py
@@ -1,20 +1,18 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
 
 def find_shortest_path(graph, start, goal):
     # print("TCL: goal", goal)
     # print("TCL: start", start)
-    print("TCL: graph", graph)
-    temp_graph = graph #unseenNodes
+    # print("TCL: graph", graph)
+    temp_graph = graph
     infinity = 999999
     path = [] 
-    cost = {} #shortest_distance
-    track = {}  #track_prodecessor
+    cost = {}
+    track = {} 
     
     for node in temp_graph:
         cost[node] = infinity
     cost[start] = 0
-    print("TCL: cost", cost)
+    # print("TCL: cost", cost)
 
     while temp_graph:
         min_cost_node = None #min_distance_node
@@ -26,13 +24,11 @@ def find_shortest_path(graph, start, goal):
                 min_cost_node = node
         
         path_option = graph[min_cost_node].items()
-        print("TCL: path_option", path_option)
-#sub_node = child_node
-# value = weight
-        for sub_node, value in path_option:
-            print("sub_node", sub_node, " || value", value, ' || cost[min_cost_node] ', cost[min_cost_node], '   || cost[sub_node]', cost[sub_node])
+    # print("TCL: path_option", path_option)
 
-            
+        for sub_node, value in path_option:
+    # print("sub_node", sub_node, " || value", value, ' || cost[min_cost_node] ', cost[min_cost_node], '   || cost[sub_node]', cost[sub_node])
+
             if value + cost[min_cost_node] < cost[sub_node]:
                 cost[sub_node] = value + cost[min_cost_node]
                 track[sub_node] = min_cost_node
@@ -40,34 +36,28 @@ def find_shortest_path(graph, start, goal):
     currentNode = goal
 
 
-
-
-    print("TCL: currentNode", currentNode)
-    print("TCL: start", start)
+#     print("TCL: currentNode", currentNode)
+#     print("TCL: start", start)
     while currentNode != start:
         try:
             
-            print("TCL: ",)
             path.insert(0, currentNode)
-            print("TCL: path", path)
-            print("TCL: track[currentNode]", track)
+#             print("TCL: path", path)
+#             print("TCL: track[currentNode]", track)
             currentNode = track[currentNode]
-            print("TCL: currentNode", currentNode)
+#             print("TCL: currentNode", currentNode)
         except KeyError:
-            print("TCL: KeyError", KeyError)
+#             print("TCL: KeyError", KeyError)
             print("TCL: Path is not reachable")
             break
     path.insert(0, start)
 
     if cost[goal] != infinity:
-        print("TCL: Shortest distance is " + str(cost[goal]))
+#         print("TCL: Shortest distance is " + str(cost[goal]))
         res_path = ' -> '.join(path)
-        print("TCL: Path " + str(res_path))
+#         print("TCL: Path " + str(res_path))
         print("Path from " + start + ' to ' + goal + ' is ' + str(res_path) + ', and have cost '+ str(cost[goal]))
         
-
-
-print("Hello world")
 file = input("What is graph file name: ")
 start = input("What is start node?: ")
 goal = input("What is goal node?: ")
@@ -87,6 +77,11 @@ for first_item in split_data:
         in_use_data[split_item[0]].update({split_item[1]: int(split_item[2])})
     else:
         in_use_data[split_item[0]] = {split_item[1]: int(split_item[2])}
-print("TCL: in_use_data", in_use_data)
+        
+    if split_item[1] in in_use_data:
+        in_use_data[split_item[1]].update({split_item[0]: int(split_item[2])})
+    else:
+        in_use_data[split_item[1]] = {split_item[0]: int(split_item[2])}
+# print("TCL: in_use_data", in_use_data)
 
-# find_shortest_path(in_use_data, start, goal)
+find_shortest_path(in_use_data, start, goal)
